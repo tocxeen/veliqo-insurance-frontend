e47e1d6ebe475bd1ca4dad2dcb8b287bf9af2305
tocxeen <richard@clicknpay.africa>
2023-10-03 02:23:02 +0200

    cursor to pointer on dropdown

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ export class SidebarComponent implements OnInit {
     { title: 'Users', icon: 'bi-people', link: '/users' },
     { title: 'Applicants', icon: 'bi-person', link: '/applicants' },
     { title: 'Transactions', icon: 'bi-coin', link: '/transactions' },
-    { title: 'Other', icon: '', link: 'other' },
+    { title: 'Policies', icon: 'bi-book', link: '/policy' },
   ];
   constructor() {}
 
